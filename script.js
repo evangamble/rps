48dc7dd94656ce063a0f687d473a304697a45ee1
evangamble <161368652+evangamble@users.noreply.github.com>
2026-03-11 11:41:17 -0500

    Add js logic

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("Hello World!")
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "scissors";
+        case 2:
+            return "paper";
+    }
+}
+
+function getHumanChoice() {
+    choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`You Tied.`);
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") 
+        || (humanChoice === "scissors" && computerChoice === "paper")) {
+            console.log(`You Win. ${humanChoice} beats ${computerChoice}`);
+            humanScore++
+    } else {
+        console.log(`You Lose. ${computerChoice} beats ${humanChoice}`);
+        computerScore++
+    }
+
+}
+
+function playGame() {
+
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (computerScore > humanScore) {
+        console.log(`You Lose. Final score: ${humanScore} to ${computerScore}`);
+    } else if (computerScore < humanScore) {
+        console.log(`You Win. Final score: ${humanScore} to ${computerScore}`);
+    } else {
+        console.log(`You Tied. Final score: ${humanScore} to ${computerScore}`);
+    }
+
+}
+
+
+playGame()
